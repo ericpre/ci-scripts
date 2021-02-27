0fac000cd2e69bdcf575c2315cb8c089007e6f2d
Eric Prestat <eric.prestat@gmail.com>
2021-02-27 19:13:23 +0000

    Try conda environment activation.

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -2,8 +2,6 @@ ENV_NAME="test_env"
 
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda update -y conda
-conda install mamba --yes
 conda create --yes --quiet --name $ENV_NAME
 mamba install --name $ENV_NAME python=$PYTHON_VERSION --yes
 mamba env update --name $ENV_NAME --file conda_environment.yml
