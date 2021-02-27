ef3f87e8f283db99c57776c9a49e54be3a734137
Eric Prestat <eric.prestat@gmail.com>
2021-02-27 16:54:56 +0000

    Use mamba instead of conda.

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,8 +1,12 @@
 ENV_NAME="test_env"
 
+conda config --add channels conda-forge
+conda config --set channel_priority strict
 conda update -y conda
-conda create --yes --quiet --name $ENV_NAME python=$PYTHON_VERSION
-conda env update --name $ENV_NAME --file conda_environment.yml
-conda env update --name $ENV_NAME --file conda_environment_dev.yml
+conda install mamba --yes
+conda create --yes --quiet --name $ENV_NAME
+mamba install --name $ENV_NAME python=$PYTHON_VERSION --yes
+mamba env update --name $ENV_NAME --file conda_environment.yml
+mamba env update --name $ENV_NAME --file conda_environment_dev.yml
 conda info
 conda list -n $ENV_NAME
